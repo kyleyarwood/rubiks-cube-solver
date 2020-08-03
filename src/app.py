acb8870a4205266d0e659ddc9407e6314365d2d7
kyleyarwood <yarwoodkyle@gmail.com>
2020-08-03 18:48:58 -0400

    Minimum viable GUI, will now return correct solution

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QCursor, QIcon, QPixmap
 from colour import Colour
 from solver import Solver
+from cube import Cube
 
 class App(QWidget):
     def __init__(self):
@@ -10,11 +11,19 @@ class App(QWidget):
         self.WINDOW_WIDTH = 8
         self.WINDOW_BAR_HEIGHT = 23+7
         self.selected_colour = Colour.W
+        self.colour_lists = [['red', 'white', 'blue'], ['orange', 'yellow', 'green']]
+        self.colour_map = {Colour.Y: "yellow", 
+                    Colour.W: "white", 
+                    Colour.G: "green",
+                    Colour.B: "blue",
+                    Colour.R: "red",
+                    Colour.O: "orange"}
         super().__init__()
         self.window = QWidget(self)
         self.twoDCubeMapLayoutLabels = []
         self.colourSelectorLayoutLabels = []
-        self.solver = None
+        self.selectedColourLabel = None
+        self.solution = None
         self.initUI()
 
     def initUI(self):
@@ -50,6 +59,7 @@ class App(QWidget):
         pixmap = pixmap.scaledToHeight(60).scaledToWidth(60)
         label.setPixmap(pixmap)
         layout.addWidget(label)
+        self.selectedColourLabel = label
         return layout
 
     def solve_button(self):
@@ -58,8 +68,31 @@ class App(QWidget):
         return button
 
     def solve_puzzle(self):
-        self.solution.setText("Hello world")
+        solver = Solver(cube=Cube(self.get_cube_state()))
+        solution = solver.solve()
+        self.solution.setText(' '.join(solution))
         self.window.repaint()
+
+    def get_cube_state(self):
+        cube_state = [[[None for i in range(3)] for j in range(3)] for k in range(6)]
+        for i,piece in enumerate(self.twoDCubeMapLayoutLabels):
+            if 0 <= i < 9:
+                cube_state[1][i%3][i//3] = piece[5]
+            elif 9 <= i < 45:
+                k = (i-9)%12
+                if 0 <= k < 3:
+                    cube_state[0][k][(i-9)//12] = piece[5]
+                elif 3 <= k < 6:
+                    cube_state[2][k-3][(i-9)//12] = piece[5]
+                elif 6 <= k < 9:
+                    cube_state[4][k-6][(i-9)//12] = piece[5]
+                else:
+                    cube_state[5][k-9][(i-9)//12] = piece[5]
+            else:
+                cube_state[3][i%3][(i-45)//3] = piece[5]
+            
+        return cube_state
+
 
     def get_solution_widget(self):
         self.solution = QLineEdit()
@@ -69,16 +102,26 @@ class App(QWidget):
     def colourSelectorLayout(self):
         layout = QHBoxLayout()
         layout.setSpacing(10)
-        for colour_list in [['red', 'white', 'blue'], ['orange', 'yellow', 'green']]:
+        horiz_gap,vert_gap,start_i,start_j = 70,170,581,50
+        cube_dim=60
+        reverse_colour_map = {name: colour for colour,name in self.colour_map.items()}
+        for i,colour_list in enumerate(self.colour_lists):
             vlayout = QVBoxLayout()
             vlayout.setSpacing(10)
-            for colour in colour_list:
+            for j,colour in enumerate(colour_list):
                 label = QLabel(self)
                 pixmap = QPixmap('../img/' + colour + '.png')
                 pixmap = pixmap.scaledToHeight(60).scaledToWidth(60)
                 label.setPixmap(pixmap)
                 vlayout.addWidget(label)
-                self.colourSelectorLayoutLabels.append(label)
+                self.colourSelectorLayoutLabels.append(
+                    [label,
+                    start_i+i*(horiz_gap),
+                    start_j+j*(vert_gap),
+                    start_i+i*(horiz_gap)+cube_dim,
+                    start_j+j*(vert_gap)+cube_dim,
+                    reverse_colour_map[colour]]
+                )
             layout.addLayout(vlayout)
         return layout
 
@@ -95,11 +138,12 @@ class App(QWidget):
                 pixmap = QPixmap('../img/black.png') if 3<=i<6 or 3<=j<6 else QPixmap('../img/grey.png')
                 if 3 <= i < 6 or 3 <= j < 6:
                     self.twoDCubeMapLayoutLabels.append(
-                        (label,
+                        [label,
                         i*(cube_width+gap)+self.WINDOW_WIDTH,
                         j*(cube_height+gap)+self.WINDOW_WIDTH,
                         i*(cube_width+gap)+cube_width+self.WINDOW_WIDTH,
-                        j*(cube_height+gap)+cube_height+self.WINDOW_WIDTH))
+                        j*(cube_height+gap)+cube_height+self.WINDOW_WIDTH,
+                        None])
                 pixmap = pixmap.scaledToHeight(40).scaledToWidth(40)
                 label.setPixmap(pixmap)
                 vlayout.addWidget(label)
@@ -115,26 +159,27 @@ class App(QWidget):
         if i is not None:
             self.set_cube_piece_colour(i)
             return
-        i = self.get_colour_selection_label(cursor_pos, window_pos)
-        if i is not None:
-            self.set_selected_colour(i)
+        colour = self.get_colour_selection(cursor_pos, window_pos)
+        if colour is not None:
+            self.set_selected_colour(colour)
 
-    def get_colour_selection_label(self, cursor_pos, window_pos):
+    def get_colour_selection(self, cursor_pos, window_pos):
+        x_rel = cursor_pos.x() - window_pos.x() - self.WINDOW_WIDTH
+        y_rel = cursor_pos.y() - window_pos.y() - self.WINDOW_BAR_HEIGHT
+        for label in self.colourSelectorLayoutLabels:
+            print(label)
+            if label[1] <= x_rel <= label[3] and label[2] <= y_rel <= label[4]:
+                return label[5]
         return None
 
     def set_cube_piece_colour(self, i):
-        colour_map = {Colour.Y: "yellow", 
-                    Colour.W: "white", 
-                    Colour.G: "green",
-                    Colour.B: "blue",
-                    Colour.R: "red",
-                    Colour.O: "orange"}
-        pixmap = QPixmap('../img/' + colour_map[self.selected_colour] + '.png')
+        pixmap = QPixmap('../img/' + self.colour_map[self.selected_colour] + '.png')
         pixmap = pixmap.scaledToHeight(40).scaledToWidth(40)
         self.twoDCubeMapLayoutLabels[i][0].setPixmap(pixmap)
+        self.twoDCubeMapLayoutLabels[i][5] = self.selected_colour
+        self.window.repaint()
 
     def get_cube_piece_label(self, cursor_pos, window_pos):
-        layout_distance = 200
         x_rel = cursor_pos.x() - window_pos.x() - self.WINDOW_WIDTH
         y_rel = cursor_pos.y() - window_pos.y() - self.WINDOW_BAR_HEIGHT
         for i,label in enumerate(self.twoDCubeMapLayoutLabels):
@@ -142,3 +187,10 @@ class App(QWidget):
                 print(i)
                 return i
         return None
+
+    def set_selected_colour(self, colour):
+        self.selected_colour = colour
+        pixmap = QPixmap("../img/" + self.colour_map[colour] + ".png")
+        pixmap = pixmap.scaledToWidth(60).scaledToHeight(60)
+        self.selectedColourLabel.setPixmap(pixmap)
+        self.window.repaint()
